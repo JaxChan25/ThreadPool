f9ce697f6fcbbabdeb67f750698f7d0e586a152f
陈亮 <>
2021-04-12 10:53:48 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/hello11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello11.dir/main.cpp.o"
+  "hello11"
+  "hello11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 来源
+ThreadPool
+
+# 目的
+学习线程池到demo，也是cmake的一次简单尝试
